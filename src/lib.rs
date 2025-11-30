1189091289910932340cbf316bb11d0d35ad82d6
fietensen <fietensen@gmail.com>
2025-12-01 00:49:03 +0100

    tests: import Game from crate root; expose model types to crate root

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,4 @@ pub mod routes;
 pub use global_state::GlobalState;
 pub use model::steamgriddb_models;
 pub use services::steamgriddb_service::SteamgriddbService;
+pub use model::{Game, GameList, AssetList, Response};
